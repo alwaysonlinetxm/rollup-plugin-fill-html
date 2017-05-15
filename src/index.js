8f6a8ea068fce755bdd7dc58ed0c8bee1cfc433b
taoxuming <taoxuming@hys-inc.cn>
2017-05-15 18:39:34 +0800

    update code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import { statSync, readFileSync, writeFileSync, readdirSync, existsSync, unlinkSync } from 'fs';
 import { relative, basename  } from 'path';
+import hasha from 'hasha';
+
+function traverse(dir, list) {
+	const dirList = readdirSync(dir);
+	dirList.forEach(node => {
+		const file = `${dir}/${node}`;
+		if (statSync(file).isDirectory()) {
+			traverse(file, list);
+		} else {
+			if (/\.js$/.test(file)) {
+				list.push(file);
+			}
+		}
+	});
+}
 
 export default (opt = {}) => {
 	const { template, filename, format } = opt;
-	const jsList = [];
-
-	function _traverse(dir) {
-		const dirList = readdirSync(dir);
-		dirList.forEach(node => {
-			const file = `${dir}/${node}`;
-			if (statSync(file).isDirectory()) {
-				_traverse(file);
-			} else {
-				if (/\.js$/.test(file)) {
-					jsList.push(file);
-				}
-			}
-		});
-	}
 
 	return {
 		name: 'html',
-		onwrite(config) {
+		onwrite(config, data) {
+			const jsList = [];
 			const file = readFileSync(template).toString();
 			const { dest, targets } = config;
 			let destPath = '';
+			let hash = '';
 
 			if (dest) {
 				 // relative(__dirname, dest) will not be equal to dest when dest is a absolute path
@@ -36,14 +38,28 @@ export default (opt = {}) => {
 					}
 				}
 			}
+			// has set hash
+			if (/\[hash\]/.test(destPath)) {
+				hash = hasha(data.code, { algorithm: 'md5' });
+				// remove the file without hash
+				unlinkSync(destPath);
+				destPath = destPath.replace('[hash]', hash);
+				// data.code will remove the last line of the source code(//# sourceMappingURL=xxx), so it's need to add this
+				writeFileSync(destPath, data.code += `//# sourceMappingURL=${basename(dest)}.map`);
+			}
 
-			const destDir = destPath.slice(0, destPath.indexOf('/'));
-			const destFile = `${destDir}/${filename || basename(template)}`;
+			const firstDir = destPath.slice(0, destPath.indexOf('/'));
+			const destFile = `${firstDir}/${filename || basename(template)}`;
 			const index = file.indexOf('</body>');
 
-			_traverse(destDir);
+			traverse(firstDir, jsList, hash);
 
-			const scripts = jsList.map(node => `<script type="text/javascript" src="${relative(destDir, node)}"></script>\n`).join('');
+			const scripts = jsList.map(node => {
+				if (node.indexOf(hash) !== -1) {
+					return `<script type="text/javascript" src="${relative(firstDir, node)}"></script>\n`;
+				}
+				return '';
+			}).join('');
 			const content = `${file.slice(0, index)}${scripts}${file.slice(index)}`;
 
 			existsSync(destFile) && unlinkSync(destFile);
