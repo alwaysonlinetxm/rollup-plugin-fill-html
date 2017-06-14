023daaa0daef39f37ed9d8b18e9b1a0ba1aa8d47
taoxuming <taoxuming@hys-inc.cn>
2017-06-14 15:06:59 +0800

    add example

---
+++ examples/rollup.config.js
@@ -0,0 +1,12 @@
+import html from 'rollup-plugin-fill-html';
+
+export default {
+  entry: 'src/index.js',
+  dest: 'dist/bundle-[hash].js',
+  format: 'iife',
+  plugins: [
+    html({
+      template: 'src/index.html'
+    })
+  ]
+};
